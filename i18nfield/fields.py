b9587ab50dad5fcb5d65acdf00f37e09e6255499
Paul Baumann <maintenance04@list-info.de>
2024-12-19 12:11:46 +0100

    .

--- i18nfield/fields.py
+++ i18nfield/fields.py
@@ -7,7 +7,7 @@ from .forms import I18nFormField, I18nTextarea, I18nTextInput
 from .strings import LazyI18nString
 
 
-class I18nFieldMixin:
+class I18nField(models.JSONField):
     form_class = I18nFormField
     widget = I18nTextInput
 
@@ -27,15 +27,15 @@ class I18nFieldMixin:
             return json.dumps({lng: value[lng] for lng, lngname in settings.LANGUAGES if value[lng]}, sort_keys=True)
         return value
 
-    def get_prep_lookup(self, lookup_type, value):  # NOQA
-        raise TypeError('Lookups on i18n strings are currently not supported.')
+    # def get_prep_lookup(self, lookup_type, value):  # NOQA
+    #     raise TypeError('Lookups on i18n strings are currently not supported.')
 
-    if django.VERSION < (2,):
-        def from_db_value(self, value, expression, connection, context):
-            return LazyI18nString(value)
-    else:
-        def from_db_value(self, value, expression, connection):
-            return LazyI18nString(value)
+    # def get_prep_lookup(self):
+    #     return [str(item) for item in self.rhs]
+    
+    def from_db_value(self, value, expression, connection):
+        value = super().from_db_value(value, expression, connection)
+        return LazyI18nString(value)
 
     def value_to_string(self, obj):
         value = self.value_from_object(obj)
@@ -43,21 +43,6 @@ class I18nFieldMixin:
 
     def formfield(self, **kwargs):
         defaults = {'form_class': self.form_class, 'widget': self.widget}
-        defaults.update(kwargs)
-        return super().formfield(**defaults)
+        return super().formfield(defaults | kwargs)
 
 
-class I18nCharField(I18nFieldMixin, models.TextField):
-    """
-    A CharField which takes internationalized data. Internally, a TextField dabase
-    field is used to store JSON. If you interact with this field, you will work
-    with LazyI18nString instances.
-    """
-    widget = I18nTextInput
-
-
-class I18nTextField(I18nFieldMixin, models.TextField):
-    """
-    Like I18nCharField, but for TextFields.
-    """
-    widget = I18nTextarea
